3d65fcfd5648e7b212916598817a70d887283260
Joel <joho.hoffmann@gmx.de>
2023-09-13 18:07:44 +0200

    merged to structure

--- controllers/generalController.go
+++ controllers/generalController.go
@@ -4,6 +4,7 @@ import (
 	"BorrowBox/database"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -73,4 +74,86 @@ func GetDocumentByIDROute(c *gin.Context) {
 		}
 	}
 	c.IndentedJSON(200, document)
+}
+
+func GetDocumentByIDROute2(c *gin.Context) {
+	collection := c.Param("collection")
+	id := c.Param("id")
+	formattedId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return
+	}
+
+	pipeline := []bson.M{
+		{
+			"$match": bson.M{"_id": formattedId},
+		},
+		{
+			"$lookup": bson.M{
+				"from":         "itemTag",
+				"localField":   "_id",
+				"foreignField": "itemId",
+				"as":           "itemTags",
+			},
+		},
+		{
+			"$lookup": bson.M{
+				"from":         "rentals",
+				"localField":   "_id",
+				"foreignField": "itemId",
+				"as":           "rentals",
+			},
+		},
+		{
+			"$lookup": bson.M{
+				"from":         "tags",
+				"localField":   "itemTags.tagId",
+				"foreignField": "_id",
+				"as":           "tags",
+			},
+		},
+		{
+			"$unwind": "$tags", // Entfalte das "tags"-Array
+		},
+		{
+			"$addFields": bson.M{
+				"rentals": bson.M{
+					"$filter": bson.M{
+						"input": "$rentals",
+						"as":    "rental",
+						"cond":  bson.M{"$eq": []interface{}{"$$rental.active", true}},
+					},
+				},
+			},
+		},
+		{
+			"$group": bson.M{
+				"_id":         "$_id",
+				"description": bson.M{"$first": "$description"},
+				"location":    bson.M{"$first": "$location"},
+				"name":        bson.M{"$first": "$name"},
+				"tagNames":    bson.M{"$push": "$tags.name"}, // Extrahiere die Tag-Namen in ein Array
+				"rentals":     bson.M{"$first": "$rentals"},  // Behalte das gefilterte "rentals"-Array bei
+			},
+		},
+		{
+			"$project": bson.M{
+				"_id":         0, // Ausblenden der _id-Felder
+				"description": 1,
+				"location":    1,
+				"name":        1,
+				"tagNames":    1, // Das Array mit Tag-Namen beibehalten
+				"rentals":     1, // Das gefilterte "rentals"-Array beibehalten
+			},
+		},
+	}
+
+	documents, err := database.NewDBAggregation(collection, pipeline)
+
+	if err != nil {
+		c.IndentedJSON(404, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(200, documents)
 }
