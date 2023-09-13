7f7aba745c1069c6aef916479efaa593ee83e738
Joel <joho.hoffmann@gmx.de>
2023-09-13 17:32:34 +0200

    restructure

---
+++ controllers/userController.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"BorrowBox/database"
+	"BorrowBox/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+)
+
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	err := database.DeleteDocument("users", id)
+	if err != nil {
+		c.IndentedJSON(404, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(200, gin.H{"message": "User deleted"})
+}
+
+func UserById(c *gin.Context) {
+	id := c.Param("id")
+	user, err := database.GetDocumentByID("users", id)
+	if err != nil {
+		c.IndentedJSON(404, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(200, user)
+}
+
+func InsertUser(c *gin.Context) {
+	var newUser models.User // Ersetze YourDataStruct mit der tatsächlichen Struktur deiner Daten
+
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+		return
+	}
+
+	err := database.InsertDocument("users", newUser)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert user"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "User inserted successfully"})
+}
+
+func UpdateUser(c *gin.Context) {
+	id := c.Param("id") // ID des zu aktualisierenden Benutzers
+
+	var updatedUser models.User // Ersetze YourDataStruct mit der tatsächlichen Struktur deiner Daten
+
+	if err := c.ShouldBindJSON(&updatedUser); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+		return
+	}
+
+	updateData := bson.M{
+		"$set": updatedUser, // Verwende die gesamte Struktur für das Update
+	}
+
+	err := database.UpdateDocument("users", id, updateData)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "User updated successfully"})
+}
---
+++ routes/routes.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"BorrowBox/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(app *gin.Engine) {
+
+	app.GET("user/:id", controllers.UserById)
+	app.GET("/:collection", controllers.GetDocuments)
+	app.DELETE("user/:id", controllers.DeleteUser)
+	app.POST("user", controllers.InsertUser)
+	app.PUT("/user/:id", controllers.UpdateUser)
+	app.GET("getDocumentByID/:collection/:id", controllers.GetDocumentByIDROute)
+	app.POST("startRental", controllers.InsertRental)
+	app.GET("useritems/:id", controllers.GetUserItems)
+	app.POST("login", controllers.Login)
+
+	app.GET("/hello", func(c *gin.Context) { // bitte nicht löschen, ist gut zum testen
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+}
